7c2d2d10861ce49301ff8b8f93a72a2754b80568
Alessandro1918 <alessandro1918@hotmail.com>
2023-02-13 16:28:15 -0300

    feat: new 'create', 'update', 'delete' routes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 4000
 
 const app = express()
 
+app.use(express.json())
 app.use("/movies", moviesRoutes)
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
